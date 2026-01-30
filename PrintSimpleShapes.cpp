0d5714ccf067d5e89078b7bb176822c3fc3f503f
Paul De Ulloa <pauld143@gmail.com>
2026-01-29 22:37:52 -0800

    firstCPPprogram

---
+++ PrintSimpleShapes.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+/*
+
+Requirements:
+- The program must produce the provided expected sample output including spacing and alignment
+- Please use only C++ input/output/formatting. C functions such as "printf" are not allowed.
+
+*/
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
